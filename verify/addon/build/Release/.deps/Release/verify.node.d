302f0507e72b5c36206d82a8a79ecbe69d7cd38f
unknown <wuzh@trgame-wuzh.uc888.cn>
2012-12-04 08:43:20 +0800

    error submit

--- verify/addon/build/Release/.deps/Release/verify.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/verify.node := ln -f "Release/obj.target/verify.node" "Release/verify.node" 2>/dev/null || (rm -rf "Release/verify.node" && cp -af "Release/obj.target/verify.node" "Release/verify.node")
--- verify/index.js
+++
@@ -1,4 +0,0 @@
-var sv = module.exports = require('./lib/verify.js');
-
-var r =sv.isArray([]);
-console.log(r)
